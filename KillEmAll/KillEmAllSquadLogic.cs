251ea97e94b67662b5c42f81774aebd62774b958
László <notisracer@gmail.com>
2019-02-28 10:10:00 +0100

    Initial commit

---
+++ KillEmAll/KillEmAllSquadLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Hackathon.Public;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.Composition;
+
+namespace KillEmAll
+{
+    [Export(typeof(ISquadLogic))]
+    [ExportMetadata("SquadName", "KillEmAll")]
+    [ExportMetadata("SquadImageResource", "KillEmAll.Resources.Face.png")]
+    public class KillEmAllSquadLogic : ISquadLogic
+    {
+        private bool _shoot = true;
+        public void Initialize(string squadId, GameOptions options)
+        {
+        }
+
+        public IEnumerable<SoldierCommand> Update(GameState state)
+        {
+            var mySoldiers = state.MySquad;
+            if (_shoot)
+            {
+                _shoot = false;
+                return mySoldiers.Select(s => new SoldierCommand() {Soldier = s, Shoot = true, MoveForward = true});
+            }
+
+            Console.ReadKey();
+            return mySoldiers.Select(s => new SoldierCommand() {Soldier = s});
+        }
+    }
+}
