d9de2d669c6ad847dcb5a755567a574052d7419f
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2019-03-06 20:51:58 +0100

    Extend TargetFinder.

--- KillEmAll/Helpers/Interfaces/ITargetFinder.cs
+++ KillEmAll/Helpers/Interfaces/ITargetFinder.cs
@@ -4,35 +4,35 @@ namespace KillEmAll.Helpers.Interfaces
 {
     public interface ITargetFinder
     {
-        /// <summary>
-        /// Returns an array of Treasures visible to the current soldier.
-        /// </summary>
-        /// <param name="currentSoldier">The reference soldier.</param>
-        /// <param name="fov">
-        ///     Optional fov, meaning only treasures that are in the soldiers 'field of view' will be returned.
-        ///     This should help in prioritizing treasures that won't require much turning to reach (it's very slow to completely turn around).
-        /// </param>
-        /// <returns>Array of treasures. Empty array if no treasures are visible.</returns>
-        Treasure[] GetVisibleTreasures(Soldier currentSoldier, float fov = 0f);
-
-        /// <summary>
-        /// Returns an array of Soldiers visible to the current soldier.
-        /// </summary>
-        /// <param name="currentSoldier">The reference soldier.</param>
-        ///  /// <param name="fov">
-        ///     Optional fov, meaning only enemies that are in the soldiers 'field of view' will be returned.
-        ///     This should help in prioritizing enemies that won't require much turning to reach (it's very slow to completely turn around).
-        /// </param>
-        /// <returns>Array of soldiers. Empty array if no soldiers are visible.</returns>
+        // ENEMY
         Soldier[] GetVisibleEnemies(Soldier currentSoldier, float fov = 0);
 
-        /// <summary>
-        /// Returns closest enemy to the current soldier.
-        /// </summary>
-        /// <param name="currentSoldier">Reference soldier.</param>
-        /// <returns>Soldier object or null if no enemies are visible.</returns>
         Soldier GetClosestVisibleEnemy(Soldier currentSoldier, float fov = 0f);
 
         Soldier GetClosestEnemyOfAll(Soldier currentSoldier);
+
+
+        // TREASURE
+        Treasure[] GetVisibleTreasures(Soldier currentSoldier, float fov = 0f);
+
+        Treasure GetClosestTreasure(Soldier currentSoldier, Treasure[] treasures);
+
+        Treasure GetClosestVisibleTreasure(Soldier currentSoldier, float fov = 0);
+
+
+        // AMMO
+        AmmoBonus[] GetVisibleAmmos(Soldier currentSoldier, float fov = 0f);
+
+        AmmoBonus GetClosestAmmo(Soldier currentSoldier, AmmoBonus[] ammos);
+
+        AmmoBonus GetClosestVisibleAmmo(Soldier currentSoldier, float fov = 0);
+
+
+        // HEALTH
+        HealthBonus[] GetVisibleHealths(Soldier currentSoldier, float fov = 0f);
+
+        HealthBonus GetClosestHealth(Soldier currentSoldier, HealthBonus[] healths);
+
+        HealthBonus GetClosestVisibleHealth(Soldier currentSoldier, float fov = 0);
     }
 }
--- KillEmAll/Helpers/TargetFinder.cs
+++ KillEmAll/Helpers/TargetFinder.cs
@@ -19,6 +19,8 @@ namespace KillEmAll.Helpers
             _movementUtility = movementUtility;
         }
 
+        /////////
+        // ENEMY
         public Soldier GetClosestVisibleEnemy(Soldier currentSoldier, float fov = 0)
         {
             var enemies = GetVisibleEnemies(currentSoldier);
@@ -68,10 +70,44 @@ namespace KillEmAll.Helpers
             return visibleEnemies;
         }
 
+
+        ////////////
+        // TREASURE
+        public Treasure GetClosestVisibleTreasure(Soldier currentSoldier, float fov = 0)
+        {
+            var treasures = GetVisibleTreasures(currentSoldier);
+            return GetClosestTreasure(currentSoldier, treasures);
+        }
+
+        public Treasure GetClosestTreasureOfAll(Soldier currentSoldier, float fov = 0)
+        {
+            var treasures = _gameStateProvider.Get().VisibleTreasures;
+            return GetClosestTreasure(currentSoldier, treasures);
+        }
+
+        public Treasure GetClosestTreasure(Soldier currentSoldier, Treasure[] treasures)
+        {
+            Treasure closestTreasure = null;
+            var closestDistance = double.MaxValue;
+            foreach (var treasure in treasures)
+            {
+                if (treasure == null)
+                    break;
+
+                var distance = _movementUtility.DistanceBetween(currentSoldier.Position, treasure.Position);
+                if (distance < closestDistance)
+                {
+                    closestTreasure = treasure;
+                    closestDistance = distance;
+                }
+            }
+            return closestTreasure;
+        }
+
         public Treasure[] GetVisibleTreasures(Soldier currentSoldier, float fov = 0)
         {
             var gameState = _gameStateProvider.Get();
-            var visibleTreasures = new Treasure[gameState.MySquad.Length * (gameState.Squads.Length - 1)];
+            var visibleTreasures = new Treasure[gameState.VisibleTreasures.Length];
 
             for (var i = 0; i < gameState.VisibleTreasures.Length; i++)
             {
@@ -84,6 +120,110 @@ namespace KillEmAll.Helpers
                 visibleTreasures[i] = target;
             }
             return visibleTreasures;
+        }
+
+
+        ///////
+        // AMMO
+        public AmmoBonus GetClosestVisibleAmmo(Soldier currentSoldier, float fov = 0)
+        {
+            var ammos = GetVisibleAmmos(currentSoldier);
+            return GetClosestAmmo(currentSoldier, ammos);
+        }
+
+        public AmmoBonus GetClosestAmmoOfAll(Soldier currentSoldier, float fov = 0)
+        {
+            var ammos = _gameStateProvider.Get().VisibleAmmoBonuses;
+            return GetClosestAmmo(currentSoldier, ammos);
+        }
+
+        public AmmoBonus GetClosestAmmo(Soldier currentSoldier, AmmoBonus[] ammos)
+        {
+            AmmoBonus closestAmmo = null;
+            var closestDistance = double.MaxValue;
+            foreach (var ammo in ammos)
+            {
+                if (ammo == null)
+                    break;
+
+                var distance = _movementUtility.DistanceBetween(currentSoldier.Position, ammo.Position);
+                if (distance < closestDistance)
+                {
+                    closestAmmo = ammo;
+                    closestDistance = distance;
+                }
+            }
+            return closestAmmo;
+        }
+
+        public AmmoBonus[] GetVisibleAmmos(Soldier currentSoldier, float fov = 0)
+        {
+            var gameState = _gameStateProvider.Get();
+            var visibleAmmos = new AmmoBonus[gameState.VisibleAmmoBonuses.Length];
+
+            for (var i = 0; i < gameState.VisibleAmmoBonuses.Length; i++)
+            {
+                var target = gameState.VisibleAmmoBonuses[i];
+                var walls = _wallMapping.GetCrossedWalls(currentSoldier.Position, target.Position);
+
+                if (walls != null && walls.Count > 0)
+                    continue;
+
+                visibleAmmos[i] = target;
+            }
+            return visibleAmmos;
+        }
+
+
+        //////////
+        // HEALTH
+        public HealthBonus GetClosestVisibleHealth(Soldier currentSoldier, float fov = 0)
+        {
+            var healths = GetVisibleHealths(currentSoldier);
+            return GetClosestHealth(currentSoldier, healths);
+        }
+
+        public HealthBonus GetClosestHealthOfAll(Soldier currentSoldier, float fov = 0)
+        {
+            var healths = _gameStateProvider.Get().VisibleHealthBonuses;
+            return GetClosestHealth(currentSoldier, healths);
+        }
+
+        public HealthBonus GetClosestHealth(Soldier currentSoldier, HealthBonus[] healths)
+        {
+            HealthBonus closestHealth = null;
+            var closestDistance = double.MaxValue;
+            foreach (var health in healths)
+            {
+                if (health == null)
+                    break;
+
+                var distance = _movementUtility.DistanceBetween(currentSoldier.Position, health.Position);
+                if (distance < closestDistance)
+                {
+                    closestHealth = health;
+                    closestDistance = distance;
+                }
+            }
+            return closestHealth;
+        }
+
+        public HealthBonus[] GetVisibleHealths(Soldier currentSoldier, float fov = 0)
+        {
+            var gameState = _gameStateProvider.Get();
+            var visibleHealths = new HealthBonus[gameState.VisibleHealthBonuses.Length];
+
+            for (var i = 0; i < gameState.VisibleHealthBonuses.Length; i++)
+            {
+                var target = gameState.VisibleHealthBonuses[i];
+                var walls = _wallMapping.GetCrossedWalls(currentSoldier.Position, target.Position);
+
+                if (walls != null && walls.Count > 0)
+                    continue;
+
+                visibleHealths[i] = target;
+            }
+            return visibleHealths;
         }
     }
 }
